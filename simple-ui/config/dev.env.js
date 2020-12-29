161128fc672b08fa9b1ed1786c7d70a08966bc61
chenbangyuan <chenbangyuan@wondersgroup.com>
2020-12-29 22:05:13 +0800

    fix

--- simple-ui/config/dev.env.js
+++ simple-ui/config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"tools"',
+  BASE_API: '"api"',
   SYSTEM_NAME: '"网约车司机查询系统"'
 })
--- simple-ui/config/prod.env.js
+++ simple-ui/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://47.106.152.238:81/tools"',
+  BASE_API: '"http://47.106.152.238:81/ai"',
   SYSTEM_NAME: '"网约车司机查询系统"'
 }
